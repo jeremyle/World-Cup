b94075204ea6a53ca084ef93c608b4037ad5bd43
JEREMY-DESKTOP\Jeremy <khanhphuong00@gmail.com>
2026-06-12 13:48:44 -0700

    Add a background

--- app/src/main/java/com/example/worldcup/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/worldcup/ui/screens/HomeScreen.kt
@@ -1,9 +1,26 @@
 package com.example.worldcup.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.worldcup.R
 
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel = viewModel()) {
 
+    Box(
+      modifier = Modifier.fillMaxSize(),
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.home_background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+    }
 }
